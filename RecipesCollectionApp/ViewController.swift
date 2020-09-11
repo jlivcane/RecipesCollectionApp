b4f03e68351a30aac752c1761e824eeb25ae5611
jlivcane <kalibrik@inbox.lv>
2020-09-11 21:31:37 +0300

    Initial Commit

---
+++ RecipesCollectionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecipesCollectionApp
+//
+//  Created by jekaterina.livcane on 11/09/2020.
+//  Copyright © 2020 jekaterina.livcane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
